3143118057e9cbd97719bf038b8efbddfe305c4a
Harsha Malya <harshamalya3@gmail.com>
2022-03-20 18:12:01 +0530

    finding grater number

--- 002_adding_numbers.py
+++ 002_adding_numbers.py
@@ -1,4 +1,11 @@
+#a=input('enter the first number:')
+#b=input('enter the second number:')
 a=int(input('enter the first number:'))
 b=int(input('enter the second number:'))
+
 sum = a + b
-print('sum:',sum)
+if type(a) is str:
+    print('sum of strings is :',sum)
+else:
+    print('sum of numbers is :', sum)
+
---
+++ 005_if_statement.py
@@ -0,0 +1,5 @@
+a = str(input('enter your name:'))
+if a == 'harsha':
+    print('wlcm to bnglr')
+else:
+    print('ur not the right user')
