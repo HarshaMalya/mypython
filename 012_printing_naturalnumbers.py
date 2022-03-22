793e47707067ca6b1a2eaf70b36372848bebc520
Harsha Malya <harshamalya3@gmail.com>
2022-03-22 13:00:48 +0530

    whileloop basics

--- 012_printing_naturalnumbers.py
+++ 012_printing_naturalnumbers.py
@@ -1,6 +1,2 @@
-n = int(input("Please Enter any Number: "))
 
-print("The List of Natural Numbers from 1", "to", n)
 
-for i in range(1, n + 1):
-    print(i, end='  ')
---
+++ 013_whileloop_basic.py
@@ -0,0 +1,9 @@
+a=int(input('enter the value of a:'))
+while a < 10:
+    print('enter the loop')
+    print('a is lesser')
+    a = a + 1
+if a == 10 :
+    print('it is the correct value')
+else :
+    print('the value is higher that 10')
